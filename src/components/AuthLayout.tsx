fa6480e629f1339fefd579a704c0cf34fe80a00b
Ashish <ashishsaud498@gmail.com>
2025-03-25 22:33:48 +0545

    add Protected component for route authentication handeling

---
+++ src/components/AuthLayout.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { RootState } from "../app/store";
+
+const Protected = ({ children, authentication = true }) => {
+  const navigate = useNavigate();
+  const [loader, setLoader] = useState<boolean>(true);
+  const authStatus = useSelector((state: RootState) => state.auth.status);
+
+  useEffect(() => {
+    if (authentication && authStatus !== authentication) {
+      navigate("/login");
+    } else if (!authentication && authStatus !== authentication) {
+      navigate("/");
+    } 
+      setLoader(false);
+  }, [navigate, authStatus, authentication]);
+
+  return loader ? <h1>Loading ...</h1> : <>{children}</>;
+};
+
+export default Protected;
