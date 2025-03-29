0142463ffe679fe6122a26ee68c08bf736d4a19a
Ashish <ashishsaud498@gmail.com>
2025-03-30 00:56:22 +0545

    Refactor routing to include AuthLayout for authentication on relevant pages

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,77 @@ import App from "./App.tsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./app/store.ts";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home.tsx";
+import { AuthLayout } from "./components/index.ts";
+import Signup from "./pages/Signup.tsx";
+import AddPost from "./pages/AddPost.tsx";
+import EditPost from "./pages/EditPost.tsx";
+import Post from "./pages/Post.tsx";
+import AllPosts from "./pages/AllPosts.tsx";
+import Login from "./pages/Login.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: (
+          <AuthLayout authentication={false}>
+            <Login />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/signup",
+        element: (
+          <AuthLayout authentication={false}>
+            <Signup />
+          </AuthLayout>
+        ),
+      },{
+        path: "/all-posts",
+        element: (
+            <AuthLayout authentication>
+                {" "}
+                <AllPosts/>
+            </AuthLayout>
+        ),
+    },
+      {
+        path: "/all-post",
+        element: (
+          <AuthLayout authentication>
+            <AddPost />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/edit-post/:slug",
+        element: (
+          <AuthLayout authentication>
+            <EditPost />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/post/:slug",
+        element: <Post />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </Provider>
   </StrictMode>
 );
